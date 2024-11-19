8786dab4726117953257590734df35a0a3578a60
Nktpamungkas <nktpamungkas28@gmail.com>
2024-11-19 14:13:14 +0700

    buka lagi tombol edit

--- schedule/pages/LihatData.php
+++ schedule/pages/LihatData.php
@@ -644,7 +644,7 @@ include('../koneksi.php');
 
 					<td style="border:1px solid;vertical-align:middle;">
 						<?php if ($_SESSION['usr'] != 'husni'): ?>
-							<!-- <a href="?p=edit_schedule&id=<?= $row_schedule['id']; ?>&typekk=NOW&activelock=true" class="button" target="_blank">Edit</a> -->
+							<a href="?p=edit_schedule&id=<?= $row_schedule['id']; ?>&typekk=NOW&activelock=true" class="button" target="_blank">Edit</a>
 							<button class="button" style="background-color: #ff004c; color: #ffffff;" onclick="showConfirmation(<?= $row_schedule['id'] ?>);">Hapus</button>
 						<?php endif; ?>
 					</td>
